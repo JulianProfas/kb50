a3e41caa2dd0c90e0db02ca53bdc6b19412a8f7e
karimbendahman <hhs@wlan174-202.wireless.hhs.nl>
2013-09-09 14:49:44 +0200

    zz

--- Checkers/Checkers/Board.h
+++ Checkers/Checkers/Board.h
@@ -6,7 +6,20 @@
 //  Copyright (c) 2013 HHS. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
 @interface Board : NSObject
+{
+    int size;
+    NSMutableArray *pieces;
+    NSMutableArray *squares;
+}
+@property(nonatomic) int size;
+@property(nonatomic,retain) NSMutableArray *pieces;
+@property(nonatomic,retain) NSMutableArray *squares;
+
+-(void)initWithSize:(int)size;
+-(void)setup;
+-(void)draw;
 
 @end
