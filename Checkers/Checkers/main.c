cd9c4c002f1b40eee48b67f252a65bef6df6c296
asoet <asoeters@MacBook-Pro-van-Allard.local>
2013-09-09 13:32:15 +0200

    Initial project

---
+++ Checkers/Checkers/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Checkers
+//
+//  Created by Allard Soeters on 09-09-13.
+//  Copyright (c) 2013 HHS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
