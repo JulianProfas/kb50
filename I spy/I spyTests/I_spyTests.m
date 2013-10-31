8e03a072bae15d2a282c232b9031c41e1ea720e5
hammersyko <Jordi@Jordis-Mac.local>
2013-10-31 13:21:54 +0100

    Test Update

--- I spy/I spyTests/I_spyTests.m
+++ I spy/I spyTests/I_spyTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Photo.h"
+#import "Color.h"
 
 @interface I_spyTests : XCTestCase
 
@@ -24,6 +26,46 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+-(void)testGenerateAnswerSets
+{
+    Photo *photo = [[Photo alloc] init];
+    
+    //code based on generateColorMatrix from Photo.m
+    //used to create the colorMatrix generateAnswerSets requires
+    double MatrixHeight = 1 / 0.025f;
+    double MatrixWidth = MatrixHeight * 3 / 4;
+    
+    NSMutableArray *matrix = [[NSMutableArray alloc] init];
+    for(int x = 0; x < MatrixWidth; ++x)
+    {
+        NSMutableArray *column = [[NSMutableArray alloc] init];
+        [matrix addObject:column];
+        for(int y = 0; y < MatrixHeight; ++y)
+        {
+            Color *color = [[Color alloc] init];
+            
+            if(y >= (MatrixHeight/2) && x < (MatrixWidth/2)){ //Top Left
+                color.colorName = @"Blue";
+            }else if(y >= (MatrixHeight/2) && x >= (MatrixWidth/2)){ //Top Right
+                color.colorName = @"Red";
+            }else if(y < (MatrixHeight/2) && x < (MatrixWidth/2)){ //Bottom Left
+                color.colorName = @"Green";
+            }else if(y < (MatrixHeight/2) && x >= (MatrixWidth/2)){ //Bottem Right
+                color.colorName = @"Yellow";
+            }
+            
+            [column addObject:color];
+            
+        }
+    }
+    
+    photo.colorMatrix = matrix;
+    
+    NSMutableOrderedSet *answerSet = [photo generateAnswerSets:@"easy"];
+    
+    
 }
 
 - (void)testExample
