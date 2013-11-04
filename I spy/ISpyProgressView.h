0b3f1cff68bb8d03d0c545807c82c3b4d19ac128
asoet <asoeters@mbp-van-allard.wireless.hhs.nl>
2013-11-04 13:33:19 +0100

    commit karim
    ispyprogressview
    uicountinglabel

--- I spy/ISpyProgressView.h
+++ I spy/ISpyProgressView.h
@@ -25,12 +25,49 @@ typedef enum {
 @property (nonatomic) NSTimeInterval time;
 @property (nonatomic, assign) UILabelPosition labelPosition;
 
+/**
+ Initializes the timerlabel with a position
+ @param label
+ Determines if the label should show or not.
+ @param _labelPosition
+ The position of the timerlabel.
+ @param _frame
+ The frame the timerlabel is in
+ 
+ @return
+ */
 -(id) initWithTimerLabel:(bool)label LabelPosition:(UILabelPosition)_labelPosition Frame:(CGRect *)_frame;
+/**
+ Starts the timer
+ */
 -(void) startTimer;
+/**
+ Stops the timer
+ */
 -(void) stopTimer;
+/**
+ Sets the timer to the initial value
+ */
 -(void) resetTimer;
+/**
+ Sets the timer to a given value.
+ @param setTime
+ The time, which the timer should be set to.
+ */
 -(void) setTime:(NSTimeInterval)setTime;
+/**
+ Increases the timer with a given value.
+ @param addTime
+ The time that should be added to the timer.
+ */
 -(void) addTime:(float) addTime;
+/**
+ Decreases the timer with a given value.
+ @param decreaseTime
+ The amount of time the timer should be decreased with.
+ 
+ @return Returns a bool
+ */
 -(bool) decreaseTime:(float)decreaseTime;
 
 @end
--- I spy/UICountingLabel.h
+++ I spy/UICountingLabel.h
@@ -25,7 +25,22 @@ typedef NSString* (^UICountingLabelFormatBlock)(float value);
 
 @property (nonatomic, copy) UICountingLabelFormatBlock formatBlock;
 @property (nonatomic, copy) void (^completionBlock)();
-
+/**
+ Calls countFrom with a given starting value and ending value, and a set duration.
+ @param startValue
+ The value which should be started counting from.
+ @param endValue
+ The value till which should be counted to.
+ */
 -(void)countFrom:(float)startValue to:(float)endValue;
+/**
+ counts from a starting value to an endingvalue in a given amount of time
+ @param startValue
+ The value which should be started counting from.
+ @param endValue
+ The value till which should be counted to.
+ @param duration
+ How much time it should take to count from start to endingvalue.
+ */
 -(void)countFrom:(float)startValue to:(float)endValue withDuration:(NSTimeInterval)duration;
 @end
